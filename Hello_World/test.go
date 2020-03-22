7cecdc2fa17242d9cb78b54e5917a13cef778329
David Dai <daiwei19971009@gmail.com>
2020-03-22 14:37:36 -0700

    first commit

---
+++ Hello_World/test.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+  //x := []int{1,2,3,4,5}
+  //x = append(x, 20)
+  //x[0] = 10
+
+  //hashmap := make(map[string]int)
+  //hashmap["a"] = 1
+  //hashmap["b"] = 2
+  //hashmap["c"] = 3
+  //hashmap["d"] = 100
+  //x := "a"
+  //fmt.Println(hashmap[x])
+
+  for i:=0; i<5; i++{
+    fmt.Println(i)
+  }
+}
