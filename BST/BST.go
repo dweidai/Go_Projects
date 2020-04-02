f8cfd8137ab6fd59a513e8c171328ab42d765512
David Dai <daiwei19971009@gmail.com>
2020-04-01 23:56:28 -0700

    BST in progress

--- BST/BST.go
+++ BST/BST.go
@@ -13,8 +13,7 @@ type Node struct{
 
 type Tree struct{
 	root *Node
-	bool small
-	bool large
+	bool small // to keep track how is the tree structured
 	size int
 }
 
@@ -23,4 +22,60 @@ func newNode(value interface{}) *Node{
 	temp := new(Node)
 	temp.value = value
 	return temp
+}
+
+func newTree(small bool) *Tree{
+	temp := new(Tree)
+	temp.small = small
+	return temp
+}
+
+func size(tree *Tree) int{
+	return tree.size
+}
+
+func insert(value interface{}, tree *Tree){
+	if size(tree) == 0{
+		tree.root = newNode(value)
+		tree.size ++
+	}else{
+		helper_insert(value, tree.root, tree.small)
+		tree.size ++
+	}
+}
+
+func helper_insert(value interface{}, node *Node, bool small){
+	if small == true{
+		if value < node.value{
+			if node.left == nil{
+				node.left = newNode(value)
+			} else {
+				helper_insert(value, node.left, small)
+			}
+		} else{
+			if node.right == nil{
+				node.right = newNode(value)
+			} else {
+				helper_insert(value, node.right, small)
+			}
+		}
+	}else{
+		if value > node.value{
+			if node.left == nil{
+				node.left = newNode(value)
+			} else{
+				helper_insert(value, node.left, small)
+			}
+		} else{
+			if node.right == nil{
+				node.right = newNode(value)
+			} else {
+				helper_insert(value, node.right, small)
+			}
+		}
+	}
+}
+
+func preorder(tree *Tree){
+
 }
