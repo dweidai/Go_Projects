d2707d35bf7f2daab2a0b9246a6e9c1a7383b32d
David Dai <daiwei19971009@gmail.com>
2020-04-05 23:58:21 -0700

    huffman in progress

--- HuffmanCoding/Huffman.go
+++ HuffmanCoding/Huffman.go
@@ -1,9 +1,26 @@
-type huffmanTree struct {
-	nodes    []huffmanNode
-	nextNode int
+package huffman
+
+import(
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+type Node struct{
+	Parent *Node
+	Left *Node
+	Right *Node
+	Count int
+	Value int
 }
 
-type huffmanNode struct {
-	left, right           uint16
-	leftValue, rightValue uint16
+
+func MakeNodes(text string) map[string]int{
+	m := make(map[string]int)
+	dat, err := ioutil.ReadFile(text)
+	data := string(dat)
+	for i,c := range data{
+		
+	}
 }
