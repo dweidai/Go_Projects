ff515240b98b56daf1d34f40035f413d9306f532
David Dai <daiwei19971009@gmail.com>
2020-04-16 14:11:22 -0700

    Updating Snake

--- Snake/main.go
+++ Snake/main.go
@@ -1,8 +1,13 @@
 package main
 
-import tl "github.com/JoelOtter/termloop"
+import (
+	"fmt"
+	"math/rand"
+	"time"
 
-var game *tl.game
+	tl "github.com/JoelOtter/termloop"
+)
+var game *tl.Game
 var field *Field
 
 func endGame(){
@@ -13,21 +18,229 @@ func endGame(){
 }
 
 func main(){
+	fmt.Println("the game is about to start")
+
+	rand.Seed(time.Now().UnixNano())
+
 	game := tl.NewGame()
 	main := tl.NewBaseLevel(tl.Cell{
 		Bg: tl.ColorBlack,
 	})
 	width := 80
 	height := 30
-	field := NewField(width, height)
-	snake := NewSnake()
-	food := NewFood()
+	field := newField(width, height)
+	snake := newSnake()
+	food := newFood(width, height)
 
 	main.AddEntity(field)
 	main.AddEntity(snake)
 	main.AddEntity(food)
 
-	game.Screen.SetLevel(main)
-	game.Screen.SetFps(10)
+	game.Screen().SetLevel(main)
+	game.Screen().SetFps(10)
 	game.Start()
+}
+
+type Node struct{
+	x int
+	y int
+}
+
+type Food struct{
+	*tl.Entity
+	location Node
+}
+
+func newFood(width int, height int) *Food{
+	f := new(Food)
+	f.Entity = tl.NewEntity(1,1,1,1)
+	f.location.x = randInRange(1, width-1)
+	f.location.y = randInRange(1, height-1)
+	return f
+}
+
+func randInRange(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func (f *Food) position()(int, int){
+	return f.location.x, f.location.y
+}
+
+func (f *Food) display(screen *tl.Screen){
+	screen.RenderCell(f.location.x, f.location.y, &tl.Cell{
+		Fg: tl.ColorRed,
+		Ch: '*',
+	})
+}
+
+func (f *Food) snakeCollision(width int, height int) {
+	f = newFood(width, height)
+}
+
+func (f *Food) collide(collision tl.Physical, width int, height int){
+	switch collision.(type){
+	case *Snake:
+		f.snakeCollision(width, height)
+	}
+}
+
+type Field struct{
+	*tl.Entity
+	width int
+	height int
+	edges map[Node]int
+}
+
+func newField(width int, height int) *Field{
+	f := new(Field)
+	f.Entity = tl.NewEntity(1, 1, 1, 1)
+	f.width = width-1
+	f.height = height-1
+	f.edges = make(map[Node]int)
+	for i := 0; i < f.width; i++ {
+		f.edges[Node{i, 0}] = 1
+		f.edges[Node{i, f.height}] = 1
+	}
+	for j:=0; j<f.height; j++{
+		f.edges[Node{0, j}] = 1
+		f.edges[Node{f.width, j}] = 1
+	}
+	return f
+}
+
+func (f *Field) update(screen *tl.Screen){
+	for c := range f.edges{
+		screen.RenderCell(c.x, c.y, &tl.Cell{
+				Bg: tl.ColorBlue,
+		})
+	}
+}
+
+func (f *Field) contain(cord Node) bool{
+	value, ok := f.edges[cord]
+	if value != 0 && value != 1{
+		return false
+	} 
+	return ok
+}
+
+const(
+	right direction  =  iota
+	left
+	up 
+	down 
+)
+
+type direction int
+
+type Snake struct{
+	*tl.Entity
+	body []Node
+	direction direction
+	length int
+}
+
+func newSnake() *Snake{
+	s := new(Snake)
+	s.Entity = tl.NewEntity(5,5,1,1)
+	s.body = []Node{ 
+		{7, 10},
+		{8, 10},
+		{9, 10},
+	}
+	s.length = len(s.body)
+	s.direction = right
+	return s
+}
+
+func (s *Snake) getHead() *Node{
+	return &s.body[s.length-1]
+}
+
+func (s *Snake) grow(){
+	s.length ++
+}
+
+func (s *Snake) collideTest(field *Field) bool{
+	if field.contain(*s.getHead()){
+		return true
+	} else{
+		for i:=0; i<s.length-1; i++{
+			if *s.getHead() == s.body[i]{
+				return true
+			}
+		}
+		return false
+	}
+	return false
+}
+func (s *Snake) collide(collision tl.Physical){
+	switch collision.(type){
+	case *Food:
+		s.foodCollision()
+	case *Field:
+		s.borderCollision() 
+	}
+}
+
+func (s *Snake) foodCollision(){
+	s.grow()
+}
+
+func (s *Snake) borderCollision(){
+	endGame()
+}
+
+func (s *Snake) update(screen *tl.Screen, field *Field){
+	newHead := *s.getHead()
+	if s.direction == right{
+		newHead.x++
+	} else if s.direction == left{
+		newHead.x--
+	} else if s.direction == up{
+		newHead.y++
+	} else{
+		newHead.y--
+	}
+	if s.length > len(s.body){
+		s.body = append(s.body, newHead)
+	}else{
+		s.body = append(s.body[1:], newHead)
+	}
+	if s.collideTest(field){
+		endGame()
+	}
+
+	for i, j:=range s.body{
+		if i >= 0{
+			screen.RenderCell(j.x, j.y, &tl.Cell{
+				Fg: tl.ColorGreen,
+				Ch: 'o',
+			})
+		}
+	}
+}
+
+func (s *Snake) keyPress(event tl.Event){
+	if event.Type == tl.EventKey{
+		switch event.Key {
+		case tl.KeyArrowRight:
+			if s.direction != left {
+				s.direction = right
+			}
+		case tl.KeyArrowLeft:
+			if s.direction != right {
+				s.direction = left
+			}
+		case tl.KeyArrowUp:
+			if s.direction != down {
+				s.direction = up
+			}
+		case tl.KeyArrowDown:
+			if s.direction != up {
+				s.direction = down
+			}
+		}
+	}
 }
