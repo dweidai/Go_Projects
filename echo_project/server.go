4e79eb3cfb86bf9ddcb4b84121e965c5b809b978
David Dai <daiwei19971009@gmail.com>
2020-07-01 15:23:57 -0700

    merge pdf init

--- echo_project/server.go
+++ echo_project/server.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"net/http"
-	
-	"github.com/labstack/echo/v4"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Route => handler
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, World!\n")
 	})
+
+	// Start server
 	e.Logger.Fatal(e.Start(":4000"))
 }
