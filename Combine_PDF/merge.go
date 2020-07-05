8773c2e3eb7353b9b72ceaa93867de347600e74d
David Dai <daiwei19971009@gmail.com>
2020-07-05 14:51:50 -0700

    messing with text convert to pdf

--- Combine_PDF/merge.go
+++ Combine_PDF/merge.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"io/ioutil"
+	"strings"
 	"fmt"
 	pdf "github.com/unidoc/unipdf/model"
+	gopdf "github.com/jung-kurt/gofpdf"
 	"os"
 )
 
@@ -30,17 +33,59 @@ func main () {
 
 	// then need to check if those files really exist
 	for i := 0; i < len(inputFiles); i++ {
+		split := strings.Split(inputFiles[i], ".")
+		extension := split[len(split)-1]
+		fmt.Println(extension)
 		if _, err := os.Stat(inputFiles[i]); os.IsNotExist(err) {
  			fmt.Println(inputFiles[i] + "\tDoes not Exist")
  			os.Exit(1)
-		}		
-		
+		} else if (extension != "pdf"){
+			fmt.Println("Converting the file to pdf file")
+			if (extension == "txt") {
+				inputFiles[i] = txtToPDF(inputFiles[i])
+			} else if (extension == "jpeg" ){
+
+			} else if (extension == "png"){
+
+			} else if (extension == "jpg"){
+
+			} else if (extension == "gif"){
+
+			}
+		}
 	}
 
 	fmt.Println("Writing to " + output)
 	mergePdf(output, inputFiles)
 }
 
+func txtToPDF(input string) string{
+	txtStr, err := ioutil.ReadFile(input)
+	if err != nil {
+		pdf.SetError(err)
+	}
+	split := strings.Split(inputFiles[i], ".")
+	split[len(split)-1] = "pdf"
+	toReturn := strings.Join(reg[:],",")
+	err = gopdf.GeneratePdf(toReturn)
+	if err != nil {
+		panic(err)
+	}
+	// Font
+	pdf.SetFont("Times", "", 12)
+	// Output text in a 6 cm width column
+	pdf.MultiCell(60, 5, string(txtStr), "", "", false)
+	pdf.Ln(-1)
+	return toReturn
+}
+
+func pictureToPDF(input string) string{
+	return input
+}
+
+func docToPDF(input string) string{
+	return input
+}
 
 func mergePdf(output string, inputFiles []string) error{
 	pdfWriter := pdf.NewPdfWriter()
--- echo_project/server.go
+++ echo_project/server.go
@@ -1,24 +1,66 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
+func upload(c echo.Context) error {
+
+	// Multipart form
+	form, err := c.MultipartForm()
+	if err != nil {
+		return err
+	}
+	files := form.File["files"]
+
+	for _, file := range files {
+		// Source
+		src, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer src.Close()
+
+		// Destination
+		dst, err := os.Create(file.Filename)
+		if err != nil {
+			return err
+		}
+		defer dst.Close()
+
+		// Copy
+		if _, err = io.Copy(dst, src); err != nil {
+			return err
+		}
+
+	}
+
+	return c.HTML(http.StatusOK, fmt.Sprintf("<p>Uploaded successfully %d files with fields name=%s and email=%s.</p>", len(files)))
+}
+
 func main() {
 	e := echo.New()
 
-	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Route => handler
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!\n")
 	})
 
-	// Start server
+	e.GET("/", func(c echo.Context) error {
+		return c.File("index.html")
+	})
+	e.GET("/file", func(c echo.Context) error {
+		return c.File("index.html")
+	})
+
 	e.Logger.Fatal(e.Start(":4000"))
 }
